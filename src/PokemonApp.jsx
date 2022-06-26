2abafbd646d72b987a3d89b6b3d250b7168d08aa
Daniel González Briseño <daniel.gb@globant.com>
2022-06-26 08:55:56 -0500

    07 - Show pokemons with pagination

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,24 +1,39 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getPokemons } from "./store/slices/pokemon/thunks";
 
 export const PokemonApp = () => {
 
   const dispatch = useDispatch();
 
+  const { isLoading, pokemons, page } = useSelector(state => state.pokemons );
+
   useEffect(() => {
     dispatch( getPokemons() );
   }, []);
 
   return (
-    <>
+    <div className="container">
       <h1>PokemonApp</h1>
       <hr />
+
+      <h2>Loading: { isLoading ? 'True' : 'False'  }</h2>
+
       <ul>
-        <li>item-1</li>
-        <li>item-2</li>
-        <li>item-3</li>
+        {
+          pokemons.map(({name}) => (
+            <li key={ name }>{ name }</li>
+          ))
+        }
       </ul>
-    </>
+
+      <button
+        className="btn"
+        onClick={ () => dispatch( getPokemons( page ) ) }
+        disabled={ isLoading }
+      >
+        Next
+      </button>
+    </div>
   );
 };
