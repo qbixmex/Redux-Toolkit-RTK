1dcd7b342440036516e2c01abc77061d3720f0a4
Daniel González Briseño <daniel.gb@globant.com>
2022-06-27 05:46:34 -0500

    09 - Consume API through RTK Query Custom Hook

--- src/store/TodoApp.jsx
+++ src/store/TodoApp.jsx
@@ -1,16 +1,44 @@
+import { useGetTodosQuery } from "./apis";
+
 export const TodoApp = () => {
+  const { data: todos = [], isLoading } = useGetTodosQuery();
+
   return (
     <div className="container">
       <h1>Todos RTK Query</h1>
       <hr />
 
-      <p className="loading"><b>Is Loading ...</b></p>
+      <p className="loading">Is Loading:&nbsp;
+        {
+          isLoading
+            ? <span className="green">True</span>
+            : <span className="red">False</span>
+        }
+      </p>
 
-      <pre>...</pre><br />
+      <pre>...</pre>
 
-      <button className="btn btn-blue">
-        Next Todo
-      </button>
+      <ul>
+        {
+          todos.map( todo => (
+            <li key={ todo.id }>
+              Status: (
+              {
+                todo.completed
+                  ? <span className="green"><b>Done</b></span>
+                  : <span className="orange"><b>Pending</b></span>
+              })&nbsp;
+              { todo.title }
+            </li>
+          ))
+        }
+      </ul>
+
+      <div className="mt-2">
+        <button className="btn btn-blue">
+          Next Todo
+        </button>
+      </div>
     </div>
   );
 };
